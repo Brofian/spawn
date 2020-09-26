e0e44f0a90b020f43e12e10423238ad6c97f4c6f
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2020-09-26 22:02:31 +0200

    [TASK] Added auto-generated db-table structure classes

--- src/webu/actions/DatabaseSetupAction.php
+++ src/webu/actions/DatabaseSetupAction.php
@@ -4,6 +4,7 @@ namespace webu\actions;
 
 use webu\system\Core\Base\Controller\ApiController;
 use webu\system\Core\Base\Custom\FileCrawler;
+use webu\system\Core\Base\Custom\FileEditor;
 use webu\system\Core\Base\Database\DatabaseTable;
 use webu\system\Core\Base\Helper\DatabaseHelper;
 use webu\system\Core\Custom\Debugger;
@@ -44,12 +45,16 @@ class DatabaseSetupAction extends ApiController {
             }
         );
 
+        $tableClasses = array();
+
         //Create all non existing tables
         $counter = 0;
         foreach($ergs as $dbclass) {
 
             /** @var DatabaseTable $c */
             $c = new $dbclass();
+            $tableClasses[] = $c;
+
             $tableName = $c->getTableName();
 
             if($dbhelper->doesTableExist($tableName)) {
@@ -63,8 +68,48 @@ class DatabaseSetupAction extends ApiController {
             $counter++;
         }
 
+
+        //create Database-Structure-Classes
+        $this->createDatabaseStructures($tableClasses);
+
         /** @var $response Response */
         $response->getTwigHelper()->setOutput("Created ".$counter." system-tables!"); ;
+    }
+
+
+    private function createDatabaseStructures(array $tables) {
+
+        $folderName = ROOT . '\\var\\cache\\database\\';
+
+        /** @var DatabaseTable $table */
+        foreach($tables as $table) {
+            $tablename = $table->getTableName();
+
+
+            $filecontent =
+"<?php
+
+namespace webu\\cache\\database\\table;
+                
+class ".$tablename." {
+    
+\tconst _TABLENAME_RAW = '".$tablename."';
+\tconst TABLENAME = '`".$tablename."`';
+            
+";
+
+            foreach($table->getColumnNames() as $columnName) {
+                $filecontent .=  "\tconst _" . strtoupper($columnName) . "_RAW = '" . $columnName . "';" . PHP_EOL;
+                $filecontent .=  "\tconst " . strtoupper($columnName) . " = '`".$tablename."`.`" . $columnName . "`';" . PHP_EOL;
+            }
+
+            $filecontent .= '}';
+
+            $fileName = $folderName . $tablename . '.php';
+            FileEditor::createFolder(dirname($fileName));
+            FileEditor::insert($fileName, $filecontent);
+        }
+
     }
 
 
