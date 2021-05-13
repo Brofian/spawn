1ab84b2a91a5f2db60c814385bf354b9f81abb0a
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2021-05-13 15:27:36 +0200

    [TASK] Adjusted config sample and added modules folder to git

--- dev/console/modules/compile-js.php
+++ dev/console/modules/compile-js.php
@@ -8,16 +8,15 @@ use webu\system\Core\Contents\Modules\ModuleLoader;
 //load resources from modules
 include(__DIR__ . "/gather-files.php");
 
+include_once(__DIR__ . "/../npm/addNodeJsToPath.php");
 
 $moduleCollection = include(__DIR__ . "/callable/list-modules.php");
-
 $errorCodeSum = 0;
+$webpackDir = ROOT . "/src/npm";
 
-IO::printLine("> compiling JavaScript", IO::YELLOW_TEXT);
 
 //javascript kompilieren
-include_once(__DIR__ . "/../npm/addNodeJsToPath.php");
-$webpackDir = ROOT . "/src/npm";
+IO::printLine("> compiling JavaScript", IO::YELLOW_TEXT);
 
 foreach($moduleCollection->getNamespaceList() as $namespace) {
 
@@ -26,7 +25,6 @@ foreach($moduleCollection->getNamespaceList() as $namespace) {
     $result = IO::execInDir("npx webpack --config webpack.config.js --progress", $webpackDir, false, $code);
     $errorCodeSum += $code;
     IO::printLine(IO::TAB . "Compiled " . $namespace);
-
 }
 
 
