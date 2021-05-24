60e00b0c32ad3ba1c82ab03d70aa10bb114f5698
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2021-05-24 22:03:11 +0200

    [FIX] Added exception for not loaded modules

--- dev/console/modules/callable/list-modules.php
+++ dev/console/modules/callable/list-modules.php
@@ -2,11 +2,21 @@
 
 use webu\system\Core\Contents\Modules\ModuleLoader;
 use webu\system\Core\Base\Helper\DatabaseHelper;
+use \bin\webu\IO;
 
 if(!isset($moduleCollection)) {
     $dbHelper = new DatabaseHelper();
     $moduleLoader = new ModuleLoader();
     $moduleCollection = $moduleLoader->loadModules($dbHelper->getConnection());
+
+
+    if(count($moduleCollection->getModuleList()) < 1) {
+        IO::print("Modules are not laoded yet! Please run ", IO::RED_TEXT);
+        IO::print("bin/console modules:refresh", IO::YELLOW_TEXT);
+        IO::print(" to load the modules to the database! Now loading modules manually...", IO::RED_TEXT);
+
+        $moduleCollection = $moduleLoader->readModules();
+    }
 }
 
 return $moduleCollection;
