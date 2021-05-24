758c434935b98dce64e083e8779d2e89864f5a9c
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2021-05-24 21:52:56 +0200

    [FIX] passed missing connection parameter to module loader function

--- dev/console/modules/callable/list-modules.php
+++ dev/console/modules/callable/list-modules.php
@@ -1,11 +1,12 @@
 <?php
 
 use webu\system\Core\Contents\Modules\ModuleLoader;
-
+use webu\system\Core\Base\Helper\DatabaseHelper;
 
 if(!isset($moduleCollection)) {
+    $dbHelper = new DatabaseHelper();
     $moduleLoader = new ModuleLoader();
-    $moduleCollection = $moduleLoader->loadModules();
+    $moduleCollection = $moduleLoader->loadModules($dbHelper->getConnection());
 }
 
 return $moduleCollection;
