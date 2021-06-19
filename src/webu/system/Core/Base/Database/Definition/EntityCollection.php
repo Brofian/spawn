e502b8f5812af35e5e0861e45bdebbf4406f1412
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2021-06-19 21:11:23 +0200

    [TASK] Added some hints for the future

--- src/webu/system/Core/Base/Database/Definition/EntityCollection.php
+++ src/webu/system/Core/Base/Database/Definition/EntityCollection.php
@@ -6,4 +6,22 @@ use webu\system\Core\Contents\Collection\Collection;
 
 class EntityCollection extends Collection {
 
+    protected string $containedEntityType;
+
+    public function __construct(string $containedEntityType)
+    {
+        $this->containedEntityType = $containedEntityType;
+    }
+
+    public function getContainedEntityType(): string {
+        return $this->getContainedEntityType();
+    }
+
+    //TODO: test this
+    public function add($value) {
+        if($value instanceof $this->containedEntityType) {
+            $this->collection[] = $value;
+        }
+    }
+
 }
