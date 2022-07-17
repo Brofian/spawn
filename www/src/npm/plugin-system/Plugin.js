ef50866a62f0e03cdb27e38261e6dfa67247244b
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2022-07-17 18:24:46 +0200

    build: updated composer files

--- www/src/npm/plugin-system/Plugin.js
+++ www/src/npm/plugin-system/Plugin.js
@@ -1,11 +1,11 @@
 export default class Plugin {
 
-
     /** @var {HTMLElement} _element*/
     _element = null;
     _$element = null;
     _pluginName = "";
     _selector = "";
+    options = {};
 
     _initialized = false;
 
@@ -21,50 +21,31 @@ export default class Plugin {
         this._$element = $element;
         this._pluginName = pluginName;
         this._selector = selector;
+        this.options = {};
 
         if(!this._initialized) {
+            this._setup();
             this.init();
             this._initialized = true;
         }
     }
 
-
-
+    _setup() {
+        // load options from dataset
+        if(typeof this.options === 'object') {
+            for(let option in this.options) {
+                if(this.options.hasOwnProperty(option)) {
+                    let prop = this.options[option];
+                    if(prop) {
+                        this.options[option] = this._element.dataset[prop];
+                    }
+                }
+            }
+        }
+    }
 
     init() {
         throw new Error(`The "init" method for the plugin "${this._pluginName}" is not defined.`);
     }
-
-
-    sendBaseAjaxRequest(url, isPost=false, postData=[], context = null, success=null, error=null) {
-        var me = this;
-
-        var method = "get";
-        if(isPost) method = "post";
-
-        if(context === null) context = me;
-
-        if(success === null) success = me.onBaseAjaxRequestSuccess.bind(me);
-        if(error === null) error = me.onBaseAjaxRequestError.bind(me);
-
-        return $.ajax({
-            url: url,
-            type: method,
-            data: postData,
-            context: context,
-            success: success,
-            error: error,
-        });
-    }
-
-    onBaseAjaxRequestSuccess() {
-        var me = this;
-        me.onBaseAjaxRequestResult();
-    }
-    onBaseAjaxRequestError() {
-        var me = this;
-        me.onBaseAjaxRequestResult();
-    }
-    onBaseAjaxRequestResult() {}
 
 }
